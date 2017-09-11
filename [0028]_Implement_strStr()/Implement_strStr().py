eaade41f29392c52212583f644c391b20c27de99
kotori233 <cycycy3333@163.com>
2017-09-11 20:29:16 +0800

    find()

---
+++ [0028]_Implement_strStr()/Implement_strStr().py
@@ -0,0 +1,11 @@
+class Solution(object):
+
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle not in haystack:
+            return -1
+        return haystack.find(needle)
