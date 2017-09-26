195d02011117b6ee56e1605f2d1c588eaf8f4bdd
kotori233 <cycycy3333@163.com>
2017-09-26 22:16:30 +0800

    集合

---
+++ [0169]_Majority_Element/Majority_Element_1.py
@@ -0,0 +1,11 @@
+class Solution(object):
+
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums_set = set(nums)
+        for i in nums_set:
+            if nums.count(i) > len(nums) // 2:
+                return i
