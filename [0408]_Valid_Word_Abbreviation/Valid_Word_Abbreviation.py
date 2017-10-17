6a4cf26744f29481607ce5593236a6d7d4e10780
kotori233 <cycycy3333@163.com>
2017-10-17 19:17:30 +0800

    指针遍历，强制末尾不为数字

---
+++ [0408]_Valid_Word_Abbreviation/Valid_Word_Abbreviation.py
@@ -0,0 +1,29 @@
+class Solution(object):
+
+    def validWordAbbreviation(self, s, abbr):
+        """
+        :type s: str
+        :type abbr: str
+        :rtype: bool
+        """
+        s += 'x'
+        abbr += 'x'
+        i = j = 0
+        while i < len(abbr):
+            tab = '0'
+            while abbr[i].isdigit():
+                tab += abbr[i]
+                i += 1
+            j += int(tab)
+            try:
+                if abbr[i] != s[j]:
+                    return False
+            except IndexError:
+                return False
+            i += 1
+            j += 1
+        try:
+            s[j]
+            return False
+        except IndexError:
+            return True
