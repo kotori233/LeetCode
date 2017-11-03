a3c6f7dc3feb0a617a10f77240e6d663039cc1e8
kotori233 <cycycy3333@163.com>
2017-11-03 15:56:19 +0800

    index()

---
+++ [0033]_Search_in_Rotated_Sorted_Array/Search_in_Rotated_Sorted_Array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        try:
+            return nums.index(target)
+        except ValueError:
+            return -1
