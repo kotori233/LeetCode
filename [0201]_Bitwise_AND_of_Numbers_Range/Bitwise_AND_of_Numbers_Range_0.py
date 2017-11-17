0a8bc7cc5d4aae35fc7f6cd8b1cd2473e6f92477
kotori233 <cycycy3333@163.com>
2017-11-17 20:01:17 +0800

    右移至相等左移归位

---
+++ [0201]_Bitwise_AND_of_Numbers_Range/Bitwise_AND_of_Numbers_Range_0.py
@@ -0,0 +1,14 @@
+class Solution(object):
+
+    def rangeBitwiseAnd(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while m != n:
+            m >>= 1
+            n >>= 1
+            count += 1
+        return m << count
