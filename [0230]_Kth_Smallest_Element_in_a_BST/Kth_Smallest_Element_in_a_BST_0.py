94677109866e11e3ce04b5343a0ba5ad2c13a3e9
kotori233 <cycycy3333@163.com>
2017-11-20 20:27:27 +0800

    迭代 栈

---
+++ [0230]_Kth_Smallest_Element_in_a_BST/Kth_Smallest_Element_in_a_BST_0.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        count = 0
+        stack = []
+        cur = root
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            count += 1
+            if count == k:
+                return cur.val
+            cur = cur.right
