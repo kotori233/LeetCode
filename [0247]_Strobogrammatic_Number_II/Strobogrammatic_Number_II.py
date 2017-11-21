9ec736e95bc6821f5eb7218f75a732a3f128ebe8
kotori233 <cycycy3333@163.com>
2017-11-21 19:54:47 +0800

    迭代，奇偶，两边夹

---
+++ [0247]_Strobogrammatic_Number_II/Strobogrammatic_Number_II.py
@@ -0,0 +1,24 @@
+class Solution(object):
+
+    def isStrobogrammatic(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = ['']
+        i = n % 2
+        if i == 1:
+            res = ['0', '1', '8']
+        while i <= n - 2:
+            temp = []
+            print('a')
+            for each in res:
+                if i != n - 2:
+                    temp.append('0' + each + '0')
+                temp.append('1' + each + '1')
+                temp.append('6' + each + '9')
+                temp.append('8' + each + '8')
+                temp.append('9' + each + '6')
+            res = temp
+            i += 2
+        return res
