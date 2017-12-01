f7d180fcb618ffbf2a548a4379d181acbd09724a
kotori233 <cycycy3333@163.com>
2017-12-01 23:02:02 +0800

    dfs

---
+++ [0323]_Number_of_Connected_Components_in_an_Undirected_Graph/Number_of_Connected_Components_in_an_Undirected_Graph.py
@@ -0,0 +1,26 @@
+class Solution(object):
+
+    def countComponents(self, n, edges):
+        """
+        :type n: int
+        :type edgest: List[List[int]]
+        :rtype: int
+        """
+        self.visited = [False for i in range(n)]
+
+        def dfs(node):
+            for i in sheet[node]:
+                if not self.visited[i]:
+                    self.visited[i] = True
+                    dfs(i)
+
+        sheet = {}
+        res = 0
+        for each in edges:
+            sheet[each[0]] = sheet.get(each[0], []) + [each[1]]
+            sheet[each[1]] = sheet.get(each[1], []) + [each[0]]
+        for i in range(n):
+            if not self.visited[i]:
+                res += 1
+                dfs(i)
+        return res
