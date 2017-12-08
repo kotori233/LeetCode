96a180cb199a38859a21af049e207a7858ccf2ec
kotori233 <cycycy3333@163.com>
2017-12-08 22:45:17 +0800

    randint(0,i)倒序交换

---
+++ [0384]_Shuffle_an_Array/Shuffle_an_Array.py
@@ -0,0 +1,33 @@
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        :rtype: List[int]
+        """
+        return self.nums
+
+    def shuffle(self):
+        """
+        Returns a random shuffling of the array.
+        :rtype: List[int]
+        """
+        import random
+
+        res = self.nums[:]
+        for i in range(len(res) - 1, 0, -1):
+            j = random.randint(0, i)
+            res[i], res[j] = res[j], res[i]
+        return res
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
