32b1b078d3301012a32e2060cb949ce20a5fdfd1
kotori233 <cycycy3333@163.com>
2017-12-19 00:02:56 +0800

    末尾hashgit push

---
+++ [0467]_Unique_Substrings_in_Wraparound_String/Unique_Substrings_in_Wraparound_String.py
@@ -0,0 +1,17 @@
+class Solution(object):
+
+    def findSubstringInWraproundString(self, p):
+        """
+        :type p: str
+        :rtype: int
+        """
+        sheet = {}
+        count = 0
+        for i in range(len(p)):
+            if i and (ord(p[i]) - ord(p[i - 1]) == 1 or
+                      (p[i] == 'a' and p[i - 1] == 'z')):
+                count += 1
+            else:
+                count = 1
+            sheet[p[i]] = max(sheet.get(p[i], 0), count)
+        return sum(sheet.values())
