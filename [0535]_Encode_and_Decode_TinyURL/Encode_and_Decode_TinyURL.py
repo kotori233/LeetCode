ea02c3cafadd26a782500c0f1fc25f777e669357
kotori233 <cycycy3333@163.com>
2018-01-02 21:49:36 +0800

    hash*2

---
+++ [0535]_Encode_and_Decode_TinyURL/Encode_and_Decode_TinyURL.py
@@ -0,0 +1,35 @@
+import random
+
+
+class Codec:
+
+    def __init__(self):
+        self.url2code = {}
+        self.code2url = {}
+        self.mask = \
+            '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    def encode(self, longUrl):
+        """Encodes a URL to a shortened URL.
+
+        :type longUrl: str
+        :rtype: str
+        """
+        while longUrl not in self.url2code:
+            code = ''.join(random.choice(self.mask) for _ in range(6))
+            if code not in self.code2url:
+                self.code2url[code] = longUrl
+                self.url2code[longUrl] = code
+        return 'http://tinyurl.com/' + self.url2code[longUrl]
+
+    def decode(self, shortUrl):
+        """Decodes a shortened URL to its original URL.
+
+        :type shortUrl: str
+        :rtype: str
+        """
+        return self.code2url[shortUrl[-6:]]
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
